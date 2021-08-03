7ffcb3464a2e256148d49546d1079bebf6efa3f7
ethanruiyu <pazzitul@163.com>
2021-08-03 11:53:05 +0800

    update commands, define mqtt adapter class

--- rcs/common/base/commands.py
+++ rcs/common/base/commands.py
@@ -1,19 +1,36 @@
 from datetime import datetime
+from abc import ABC
 
 
-class InitPose:
+class BaseCommand(ABC):
+    def __init__(self):
+        self._message_id = 0
+
+    @property
+    def message_id(self):
+        return self.message_id
+
+    @message_id.setter
+    def message_id(self, value: int):
+        self._message_id += value
+
+
+class InitPose(BaseCommand):
     def __init__(self, position, orientation):
+        super(InitPose, self).__init__()
         self._position = position,
         self._orientation = orientation
         self._timestamp = datetime.now().timestamp()
 
 
-class Pause:
+class Pause(BaseCommand):
     def __init__(self):
+        super(Pause, self).__init__()
         self._timestamp = datetime.now().timestamp()
 
 
-class Continue:
-    def __int__(self):
+class Continue(BaseCommand):
+    def __init__(self):
+        super(Continue, self).__init__()
         self._timestamp = datetime.now().timestamp()
 
